438a3ae546378f406a722a43eae7c67f9e63110b
Joe Santos <jaysantos012@gmail.com>
2021-06-07 23:54:47 -0400

    added the marked features and added the toggle button

--- app.js
+++ app.js
@@ -2,8 +2,12 @@ const notesEl = document.querySelector('.notes')
 const editBtn = document.querySelector('.edit');
 const deleteBtn = document.querySelector('.delete');
 
+const main = notesEl.querySelector('.main');
+const textArea = notesEl.querySelector('textarea');
+
 editBtn.addEventListener('click' || 'touchstart', (e) => {
 
-    notesEl
+    main.classList.toggle('hidden');
+    textArea.classList.toggle('hidden')
 
 })
