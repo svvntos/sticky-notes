c2a334e859d3fb4329db49615999858f5ad54998
Joe Santos <jaysantos012@gmail.com>
2021-06-08 01:23:31 -0400

    finish witht eh display of the acutlly note. now working on the add note button and function to add a new note to the screen

--- app.js
+++ app.js
@@ -5,9 +5,26 @@ const deleteBtn = document.querySelector('.delete');
 const main = notesEl.querySelector('.main');
 const textArea = notesEl.querySelector('textarea');
 
-editBtn.addEventListener('click' || 'touchstart', (e) => {
+editBtn.addEventListener('click' || 'touchstart', () => {
 
     main.classList.toggle('hidden');
     textArea.classList.toggle('hidden')
 
 })
+
+textArea.addEventListener('input', (e) => {
+    const { value } = e.target;
+
+    main.innerHTML = marked(value);
+})
+
+
+
+// <div class="notes">
+//       <div class="tools">
+//         <button class="edit"><i class="fas fa-edit"></i></button>
+//         <button class="delete"><i class="fas fa-trash-alt"></i></button>
+//       </div>
+//       <div class="main hidden"></div>
+//       <textarea></textarea>
+//     </div>
