ee15ddd5c32dc565724e291d9cbb3f414508717d
Joe Santos <jaysantos012@gmail.com>
2021-06-07 22:30:31 -0400

    first commit, creating a simple CRUD sticky notes. Added html with links and all. now working on the Js fucntion

---
+++ app.js
@@ -0,0 +1,9 @@
+const notesEl = document.querySelector('.notes')
+const editBtn = document.querySelector('.edit');
+const deleteBtn = document.querySelector('.delete');
+
+editBtn.addEventListener('click' || 'touchstart', (e) => {
+
+    notesEl
+
+})
