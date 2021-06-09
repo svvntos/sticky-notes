4758a631f0e382781ed034bb6035e029db05296c
Joe Santos <jaysantos012@gmail.com>
2021-06-09 02:06:56 -0400

    finished adding all local storage. ready to go live

--- app.js
+++ app.js
@@ -1,12 +1,18 @@
-
-
 const addBtn = document.getElementById('add');
+const notes = JSON.parse(localStorage.getItem("notes"));
+
+if (notes) {
+    notes.forEach((note) => {
+        addNewNote(note)
+    });
+}
+
 
 addBtn.addEventListener('click' || 'touchstart', () => {
     addNewNote();
 })
 
-function addNewNote() {
+function addNewNote(text = "") {
     const note = document.createElement('div');
     note.classList.add('note');
 
@@ -16,8 +22,8 @@ function addNewNote() {
         <button class="edit"><i class="fas fa-edit"></i></button>
         <button class="delete"><i class="fas fa-trash-alt"></i></button>
       </div>
-      <div class="main hidden"></div>
-      <textarea></textarea>
+      <div class="main ${text ? "" : "hidden"}"></div>
+      <textarea class="${text ? "hidden" : ""}"></textarea>
     </div>
     `;
 
@@ -26,6 +32,9 @@ function addNewNote() {
 
     const main = note.querySelector('.main');
     const textArea = note.querySelector('textarea');
+
+    textArea.value = text;
+    main.innerHTML = marked(text);
 
     editBtn.addEventListener('click' || 'touchstart', () => {
 
@@ -36,6 +45,8 @@ function addNewNote() {
 
     deleteBtn.addEventListener('click' || 'touchstart', () => {
         note.remove();
+
+        updateLS();
     })
 
 
@@ -44,10 +55,24 @@ function addNewNote() {
         const { value } = e.target;
     
         main.innerHTML = marked(value);
-    })
+
+        updateLS();
+    });
 
     document.body.appendChild(note);
 
+}
+
+function updateLS() {
+    const notesText = document.querySelectorAll('textarea');
+
+    const notes = [];
+
+    notesText.forEach((note) => {
+        notes.push(note.value);
+    });
+
+    localStorage.setItem('notes', JSON.stringify(notes));
 }
 
 
